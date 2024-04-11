1777212553761424cab74f1e0a72398f877bc3cd
OberskyiVitalii <vitalii.v.oberskyi@ukd.edu.ua>
2024-04-12 00:30:27 +0300

    sd-9

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,10 +2,15 @@ from django.urls import path
 
 from . import views
 
-app_name = "polls"
+app_name = 'polls'
 urlpatterns = [
-    path("", views.IndexView.as_view(), name="index"),
-    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
-    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
-    path("<int:question_id>/vote/", views.vote, name="vote"),
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('create/', views.create_question, name='create'),
+    path('<int:question_id>/update/', views.update_question, name='update_question'),
+    path('<int:question_id>/delete/', views.delete_question, name='delete_question'),
+    path('<int:question_id>/create_choices/', views.create_choice, name='create_choice'),
+    path('<int:question_id>/delete_choice/<int:choice_id>/', views.delete_choice, name='delete_choice'),
 ]
